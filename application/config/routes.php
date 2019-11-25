15c162d007706f75bc36f719ed51e9c0ca15760b
Reynaldi <38524172+reynaldiRepo@users.noreply.github.com>
2019-11-25 16:34:16 +0700

    mirror update

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,13 +1,11 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-
-$route = [
-    'default_controller'                => 'asrama',
-    'logout'                            => 'asrama/logout',
-    'Admin'                             =>'Admin',
-    'User'                              =>'User',
-    'Musahil'                           =>'Musahil'
-];
+$route['default_controller'] = 'asrama';
+$route['logout'] = 'asrama/logout';
+$route['admin'] = 'admin/(:any)';
+$route['user'] = 'user/(:any)';
+$route['musahil'] = 'musahil/(:any)';
+$route['musahil/insert_penghuni'] = 'Admin/insert_penghuni';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
--- application/views/Admin/insert_musahil.php
+++ application/views/Admin/insert_musahil.php
@@ -10,47 +10,47 @@ $mylevel = "Admin";
 }
 ?>
 <div class="app-main__inner">
-    <div class="app-page-title">
-        <div class="page-title-wrapper">
-            <div class="page-title-heading">
-                <div class="page-title-icon">
-                    <i class="pe-7s-home icon-gradient bg-tempting-azure">
-                    </i>
-                </div>
-                <div><?= $menu ?>
-                    <div class="page-title-subheading">Insert Data Penghuni
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="main-card mb-3 card">
-        <div class="card-body">
-            <div id="example_wrapper" class="dataTables_wrapper dt-bootstrap4">
-                <div class="row">
-                    <div class="col-sm-12">
-                        <?= $this->session->flashdata('message'); ?>
-                        <h2 class="card-title">Masukan Musahil Baru</h2>
-                        <div class="form-group">
-                            <?php echo form_open_multipart("$mylevel/add_musahil")?>
-                            <label>Nim Mahasiswa</label>
-                            <input required type="text" class="form-control" name="nimMus" id="nimMus">
-                            <br>
-                            <label>Photo Profile</label><br>
-                            <input required type="file" class="" name="file">
-                            <br><br>
-                            <label>Password</label>
-                            <input required type="password" class="form-control" name="password1">
-                            <br>
-                            <label>re-Type Password</label>
-                            <input required type="password" class="form-control" name="password2">
-                            <br>
-                            <input type="submit" class="btn btn-info btn-lg" style="width:100%">
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+	<div class="app-page-title">
+		<div class="page-title-wrapper">
+			<div class="page-title-heading">
+				<div class="page-title-icon">
+					<i class="pe-7s-home icon-gradient bg-tempting-azure">
+					</i>
+				</div>
+				<div><?= $menu ?>
+					<div class="page-title-subheading">Insert Data Penghuni
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="main-card mb-3 card">
+		<div class="card-body">
+			<div id="example_wrapper" class="dataTables_wrapper dt-bootstrap4">
+				<div class="row">
+					<div class="col-sm-12">
+						<?= $this->session->flashdata('message'); ?>
+						<h2 class="card-title">Masukan Musahil Baru</h2>
+						<div class="form-group">
+							<?php echo form_open_multipart("$mylevel/add_musahil")?>
+							<label>Nim Mahasiswa</label>
+							<input required type="text" class="form-control" name="nimMus" id="nimMus">
+							<br>
+							<label>Photo Profile</label><br>
+							<input required type="file" class="" name="file">
+							<br><br>
+							<label>Password</label>
+							<input required type="password" class="form-control" name="password1">
+							<br>
+							<label>re-Type Password</label>
+							<input required type="password" class="form-control" name="password2">
+							<br>
+							<input type="submit" class="btn btn-info btn-lg" style="width:100%">
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
